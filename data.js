2823e30e4766880c8b5b24887813baa81a485ac0
llanosplainreads <llanosplainreads@gmail.com>
2026-06-21 19:23:39 +0200

    Update data.js

--- data.js
+++ data.js
@@ -14,3 +14,22 @@ const lastRead = {
   comment: "Una lectura adictiva y perfecta para desconectar.",
   cover: "images/last-read.jpg"
 };
+
+const favourites = [
+  {
+    title: "Han cantado bingo",
+    cover: "images/han-cantado-bingo.jpg"
+  },
+  {
+    title: "Misha Zhukov debe morir",
+    cover: "images/misha-zhukov.jpg"
+  },
+  {
+    title: "El caballero y la polilla",
+    cover: "images/caballero-polilla.jpg"
+  },
+  {
+    title: "Reliquia",
+    cover: "images/reliquia.jpg"
+  }
+];
