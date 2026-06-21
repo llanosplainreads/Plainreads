132ffda78d3af02d61f585c5ad54edce89723e18
llanosplainreads <llanosplainreads@gmail.com>
2026-06-21 23:59:53 +0200

    library.js

---
+++ library.js
@@ -0,0 +1,59 @@
+const books = {
+
+caballeroPolilla: {
+title: "El caballero y la polilla",
+author: "Rachel Gillig",
+pages: 432,
+cover: "images/elcaballeroylapolilla.jpg",
+genres: ["Fantasy", "Romance", "Gothic"]
+},
+
+bourbonLies: {
+title: "Bourbon & Lies",
+author: "Victoria Wilder",
+pages: 384,
+cover: "images/bourbonandlies.jpg",
+genres: ["Romance", "Contemporary"]
+},
+
+hanCantadoBingo: {
+title: "Han cantado bingo",
+author: Lana Corujo,
+pages: 184,
+cover: "images/hancantadobingo.jpg",
+genres: ["Contemporary", "Literary Fiction"]
+},
+
+mishaZhukov: {
+title: "Misha Zhukov debe morir",
+author: "Myrlan M. Lejardi",
+pages: null,
+cover: "images/mishazhukov.jpg",
+genres: ["Romance", "Contemporary"]
+},
+
+reliquia: {
+title: "Reliquia",
+author: null,
+pages: null,
+cover: "images/reliquia.jpeg",
+genres: ["Fantasy"]
+},
+
+entreDosFuegos: {
+title: "Entre dos fuegos",
+author: "Christopher Buehlman",
+pages: 432,
+cover: "images/entredosfuegos.jpg",
+genres: ["Fantasy", "Horror", "Historical"]
+},
+
+altasangre: {
+title: "Altasangre",
+author: Claudia Amador,
+pages: 184,
+cover: "images/altasangre.jpg",
+genres: ["Fantasy"]
+}
+
+};
