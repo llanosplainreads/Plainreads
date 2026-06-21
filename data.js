f04dd2b702b151b6b5dbac60e9c4586b90f57b6c
llanosplainreads <llanosplainreads@gmail.com>
2026-06-21 19:20:41 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const currentBook = {
+  title: "El caballero y la polilla",
+  author: "Rachel Gillig",
+  pagesRead: 357,
+  totalPages: 432,
+  progress: 83,
+  cover: "images/currently-reading.jpg"
+};
+
+const lastRead = {
+  title: "Bourbon & Lies",
+  author: "Victoria Wilder",
+  rating: "⭐⭐⭐☆☆",
+  comment: "Una lectura adictiva y perfecta para desconectar.",
+  cover: "images/last-read.jpg"
+};
